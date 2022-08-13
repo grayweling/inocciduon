2b16f504f6c310cf88fde6db23724ce8ae67304e
erockenmeyer <erikrockenmeyer@gmail.com>
2022-08-12 22:27:19 -0500

    created basic server.js file

---
+++ server/server.js
@@ -0,0 +1,42 @@
+// import bits that make the server go
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+const { Server } = require('http');
+
+// here is where the internal connections will go
+
+// create server stuff
+const app = express();
+const PORT = process.env.PORT || 3001;
+/* const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+}) */
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// serve client/build as static in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+// app.use(routes); ?
+
+// create apollo server w graphql schema
+const startApolloServer = async (typeDefs, resolvers) => {
+    await server.start();
+    server.applyMiddleware({ app });
+
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`API server running on port ${PORT}!`);
+            console.log(`Use GraphQL at https://localhost:${PORT}${server.graphqlPath}`);
+        })
+    })
+}
+
+// call function to start server
+startApolloServer(typeDefs, resolvers);
