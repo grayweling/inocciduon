7d69007cde25d0697eb0a18d62945a8be2928d62
erockenmeyer <erikrockenmeyer@gmail.com>
2023-03-06 16:13:03 -0600

    started header

---
+++ client/src/component/Header/index.js
@@ -0,0 +1,24 @@
+// imports
+import React from "react";
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    // logout function
+    
+    // give us what we want to see
+    return (
+        <header>
+            <div>
+                <Link>
+                    <h1>Inocciduon</h1>
+                </Link>
+
+                <nav>
+
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
